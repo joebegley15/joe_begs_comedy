07dc4b16a519f3ce549de8a4f891b6b01f66f214
Joe Begley <joebegley15@gmail.com>
2019-05-03 01:08:38 -0700

    Laid the groundwork to put Redux in place

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,46 @@
+import React, { Component, Fragment } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from "reactstrap";
+
+class AppNavbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+  toggle() {
+    const isOpen = !this.state.isOpen;
+    this.setState({ isOpen });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">Joe Begley Comedy</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="google.com">Google</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar;
---
+++ client/src/components/Shows.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Shows extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shows: []
+    };
+  }
+  render() {
+    return <div>Shows Will Go Here</div>;
+  }
+}
+
+export default Shows;
